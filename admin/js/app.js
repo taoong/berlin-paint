081671418c37aabe7e23f7ae89bbef3d53392092
Tao Ong <taoong@berkeley.edu>
2017-12-28 21:09:23 +0800

    Uploading files

---
+++ admin/js/app.js
@@ -0,0 +1,52 @@
+// Initialize Firebase
+
+(function() {
+  var config = {
+    apiKey: "AIzaSyAAc3bIDjN1BBAajWvD7aIG2sz0z8UadHM",
+    authDomain: "berlin-83c24.firebaseapp.com",
+    databaseURL: "https://berlin-83c24.firebaseio.com",
+    projectId: "berlin-83c24",
+    storageBucket: "berlin-83c24.appspot.com",
+    messagingSenderId: "301683329371"
+  };
+  firebase.initializeApp(config);
+
+  // Get elements
+  const txtEmail = document.getElementById('txtEmail');
+  const txtPassword = document.getElementById('txtPassword');
+  const btnLogin = document.getElementById('btnLogin');
+  const btnSignup = document.getElementById('btnSignup');
+
+  // Add login event
+  btnLogin.addEventListener('click', e => {
+    // Get email and pass
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+    // Sign in
+    const promise = auth.signInWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+  })
+
+  // Add signup event
+  btnSignup.addEventListener('click', e => {
+    const email = txtEmail.value;
+    const pass = txtPassword.value;
+    const auth = firebase.auth();
+    // Sign in
+    const promise = auth.createUserWithEmailAndPassword(email, pass);
+    promise.catch(e => console.log(e.message));
+  })
+
+  // Add a realtime listener
+  firebase.auth().onAuthStateChanged(firebaseUser => {
+    if (firebaseUser) {
+        console.log(firebaseUser);
+        window.location.replace("index.html");
+    } else {
+        console.log('Not logged in');
+    }
+  })
+
+}());
+  
