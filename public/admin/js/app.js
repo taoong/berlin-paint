35a8b07cd1404961f9d9fa4d5f0c8c7ace7c2942
Tao Ong <taoong@berkeley.edu>
2018-01-13 13:51:46 -0800

    Implemented order adding form

--- public/admin/js/app.js
+++ public/admin/js/app.js
@@ -16,25 +16,43 @@
   var ordersTable = $('#orders').DataTable();
 
   dbOrders.on("child_added", snap => {
-    var dataset = [snap.child("item_id").val(), snap.child("name").val(), snap.child("type").val(), snap.child("order_status").val(), snap.child("completed").val()];
+    var dataset = [snap.child("order_date").val(), snap.child("item_id").val(), snap.child("name").val(), snap.child("quantity").val(), snap.child("type").val(), snap.child("order_status").val()];
     ordersTable.rows.add([dataset]).draw();
   });
+
+
 
   angular
     .module("app", ['firebase'])
     .controller("myCtrl", function($scope) {
-        $scope.addItem = function () {
-            $scope.errortext = "";
-            if ($scope.orders.indexOf($scope.addMe) == -1) {
-                $scope.orders.push([$scope.orderID, $scope.orderName, $scope.orderType, $scope.orderStatus, $scope.orderCompleted]);
-            } else {
-                $scope.errortext = "The item is already in your shopping list.";
-            }
+      $scope.addItem = function () {
+
+        if ($scope.itemID == null || $scope.itemName == null || $scope.itemType == null || $scope.orderStatus == null || $scope.itemQuantity == null || $scope.orderDescription == null) {
+          alert('All text fields must be filled out.');
+          return;
         }
-        $scope.removeItem = function (x) {
-            $scope.errortext = "";
-            $scope.orders.splice(x, 1);
-        } 
+
+        var orderID = dbOrders.push().key;
+        var data = {
+          order_date: Date.now(),
+          item_id: $scope.itemID,
+          name: $scope.itemName,
+          quantity: $scope.itemQuantity,
+          type: $scope.itemType,
+          order_status: $scope.orderStatus,
+          description: $scope.orderDescription,
+          reviewed: false
+        }
+        var updates = {};
+        updates['/orders/' + orderID] = data;
+        firebase.database().ref().update(updates);
+        alert('The order was added to the database.');
+        window.location.reload();
+      }
+      $scope.removeItem = function (x) {
+        $scope.errortext = "";
+        $scope.orders.splice(x, 1);
+      } 
     });
 
 }());
